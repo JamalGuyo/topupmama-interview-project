11b945a8b1dac78032d4400cddcb547cd90d5ed8
JamalGuyo <jamalguyo08@gmail.com>
2022-03-27 04:38:06 +0300

    create user details template

--- src/app/modules/users/components/user-detail/user-detail.component.ts
+++ src/app/modules/users/components/user-detail/user-detail.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent implements OnInit {
+  user = {
+    name: 'John Doe',
+    title: 'Frontend Developer',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
