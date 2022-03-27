66a3e9c28261463b89053e0ee09f78b1f4ca69ce
JamalGuyo <jamalguyo08@gmail.com>
2022-03-27 21:58:30 +0300

    validate login form and get values on submit

--- src/app/auth/component/login/login.component.ts
+++ src/app/auth/component/login/login.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmedValidator } from '@shared/utils/password-compare';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  fieldTextType: boolean;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initLoginForm();
   }
 
+  private initLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^((?=.*?[a-z])(?=.*?[0-9]))(?=.*?[#?!@$%^&*-]).{8,}$'
+          ),
+        ],
+      ],
+    });
+  }
+
+  loginUser() {
+    if (this.loginForm.invalid) return;
+    // TODO:
+    // send data to backend to register and navigate to main page
+  }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
 }
