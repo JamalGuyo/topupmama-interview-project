c31a61e14858f8c350b30c4c4c832ddf6782572f
JamalGuyo <jamalguyo08@gmail.com>
2022-03-27 21:49:31 +0300

    validate registration form and get values on submit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // created modules
 import { AuthRoutingModule } from './auth-routing.module';
@@ -10,6 +11,6 @@ import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/component/register/register.component.ts
+++ src/app/auth/component/register/register.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ConfirmedValidator } from '@shared/utils/password-compare';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  fieldTextType: boolean;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initRegisterForm();
   }
 
+  private initRegisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              '^((?=.*?[a-z])(?=.*?[0-9]))(?=.*?[#?!@$%^&*-]).{8,}$'
+            ),
+          ],
+        ],
+        password2: ['', [Validators.required]],
+      },
+      {
+        validator: ConfirmedValidator('password', 'password2'),
+      }
+    );
+  }
+
+  registerUser() {
+    if (this.registerForm.invalid) return;
+    // TODO:
+    // send data to backend to register and navigate to main page
+  }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
 }
